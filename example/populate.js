948a4ffee876ded1028846b7786c1b56ced43baa
James Halliday <mail@substack.net>
2015-03-24 01:56:06 -0700

    basic add() probably doesn't work

--- example/populate.js
+++ example/populate.js
@@ -2,19 +2,18 @@ var mddf = require('../');
 var fs = require('fs');
 
 var fd = fs.openSync('data.mddf', 'w+');
+var stat = fs.fstatSync(fd);
+
 var df = mddf({
-    size: 1024,
-    B: 8,
-    read: function (offset, size, cb) {
-        fs.read(fd, offset, size, cb);
-    },
-    write: function (block, offset, cb) {
-        fs.write(fd, block, offset, cb);
-    }
+    blksize: 4096,
+    dim: 3,
+    size: stat.size,
+    read: fs.read.bind(null, fd),
+    write: fs.write.bind(null, fd)
 });
 
-df.add([1,2,3], Buffer('abcdefghi'));
-df.add([4,5,6], Buffer(400));
-df.add([0,-2.5,3], Buffer(2000));
-df.add([-2,3,0], Buffer(1000));
-df.add([3,2,1], 'whatever');
+df.add([1,2,3], 1);
+df.add([4,5,6], 2);
+df.add([0,-2.5,3],3);
+df.add([-2,3,0], 4);
+df.add([3,2,1], 5);
--- index.js
+++ index.js
@@ -1,35 +1,79 @@
+var fs = require('fs');
 var Buffer = require('buffer').Buffer;
-var pbuf = require('protocol-buffers');
 
 module.exports = MDDF;
 
 function MDDF (opts) {
     if (!(this instanceof MDDF)) return new MDDF(opts);
-    this.B = opts.B || 8;
+    var self = this;
     this._read = opts.read;
     this._write = opts.write;
+    
+    this.blksize = opts.blksize || 4096;
+    this.dim = opts.dim;
+    this.B = Math.floor((this.blksize - 4) / (this.dim * 4 + 4));
+    this.size = opts.size || 0;
 }
 
-MDDF.prototype.add = function (pt, data, cb) {
-    if (!this._write) {
-        throw new Error('cannot add points: no write function defined');
+MDDF.prototype.add = function (pt, dataix, cb) {
+    if (!cb) cb = function () {};
+    var self = this;
+    if (!self._write) {
+        return cb(new Error('cannot add points: no write function defined'));
     }
     if (typeof data === 'string') {
         data = Buffer(data);
     }
-    this._indexOf(pt, function (err, ix) {
-        
-    });
-};
-
-MDDF.prototype._indexOf = function (pt, cb) {
-    var self = this;
-    this._readBlock(0, function (err, buf) {
-        
-    });
+    if (self.dim !== pt.length) {
+        return cb(new Error('inconsistent dimension'));
+    }
+    
+    (function next (index, depth) {
+        self._readBlock(index, function (err, buf) {
+            if (err) return cb(err);
+            var len = buf.readUInt32BE(0);
+            
+            if (len < self.B) {
+                // not full, add point
+                buf.writeUInt32BE(0, len + 1);
+                for (var i = 0; i < pt.length; i++) {
+                    buf.writeFloatBE(4 + i * (self.dim * 4 + 4), pt[i]);
+                }
+                buf.writeUInt32BE(4 + i * (self.dim * 4 + 4), dataix);
+                return self._writeBlock(index, buf, cb);
+            }
+            
+            var ix = depth % self.dim;
+            var pivot = buf.readFloatBE(4 + ix * (self.dim * 4 + 4));
+            
+            if (pt[ix] < pivot) {
+                next(index * 2 + 1, depth + 1);
+            }
+            else {
+                next((index + 1) * 2, depth + 1);
+            }
+        });
+    })(0, 0);
 };
 
 MDDF.prototype._readBlock = function (n, cb) {
+    var self = this;
+    var offset = n * this.blksize;
+    if (offset >= this.size) {
+        this.size = (n + 1) * this.blksize;
+        var buf = Buffer(this.blksize);
+        buf.writeUInt32BE(0, 0);
+        return cb(null, buf);
+    }
+    var buf = Buffer(this.blksize);
+    this._read(buf, offset, this.blksize, 0, null, function (err, buf) {
+        if (err) return cb(err);
+        else cb(err);
+    });
+};
+
+MDDF.prototype._writeBlock = function (n, buf, cb) {
+    this._write(buf, n * this.blksize, this.blksize, cb);
 };
 
 MDDF.prototype.knn = function (pt, k, maxDistance, cb) {
@@ -41,5 +85,4 @@ MDDF.prototype.nn = function (pt, maxDistance, cb) {
 };
 
 MDDF.prototype.rnn = function (pt, radius, visit) {
-    
 };
