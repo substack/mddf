ac211fcb231cd3584d25cef25b2c4615e928cf01
James Halliday <mail@substack.net>
2015-04-04 02:16:58 -0700

    hypothetical next() iterator example

---
+++ example/near.js
@@ -0,0 +1,21 @@
+var mddf = require('../');
+var fs = require('fs');
+
+var fd = fs.openSync('data.mddf', 'r');
+var stat = fs.fstatSync(fd);
+
+var df = mddf({
+    blksize: 4096,
+    dim: 3,
+    size: stat.size,
+    read: fs.read.bind(null, fd)
+});
+
+var near = df.near(process.argv.slice(2));
+(function next () {
+    near.next(function (err, pt, data) {
+        if (!pt) return;
+        console.log(pt);
+        next();
+    });
+})();
